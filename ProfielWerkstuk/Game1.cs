3c517f4343c74391b8fe5935ece06149923b1bd4
Obliviadge <jimvanvliet98@gmail.com>
2016-10-14 15:05:54 +0200

    Started working on UI and got grid drawing working.

--- ProfielWerkstuk/Game1.cs
+++ ProfielWerkstuk/Game1.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using ProfielWerkstuk.Scripts;
+using System.Windows.Forms;
 
 
 //Mijn profielwerkstuk
@@ -13,6 +16,11 @@ namespace ProfielWerkstuk
 	{
 		GraphicsDeviceManager graphics;
 		SpriteBatch spriteBatch;
+		Camera2D camera;
+		private MouseState oldState;
+		Vector2 previousMouse = new Vector2();
+		Grid grid;
+		SpriteFont font;
 
 		public Game1()
 		{
@@ -28,9 +36,26 @@ namespace ProfielWerkstuk
 		/// </summary>
 		protected override void Initialize()
 		{
-			// TODO: Add your initialization logic here
+			IsMouseVisible = true;
+			grid = new Grid(this, 64, 30, 20, 3);
 
+			// TODO: Add your initialization logic here
+			grid.generateTextures();
 			base.Initialize();
+
+			Form form = (Form)Form.FromHandle(Window.Handle);
+			form.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+
+			//Match the resolution to the resolution of the screen.
+			graphics.PreferredBackBufferWidth = form.ClientSize.Width;
+			graphics.PreferredBackBufferHeight = form.ClientSize.Height;
+			this.Window.AllowUserResizing = true;
+			graphics.ApplyChanges();
+
+			//Create camera
+			camera = new Camera2D(GraphicsDevice);
+			camera.MinimumZoom = 0.7f;
+			camera.MaximumZoom = 3f;
 		}
 
 		/// <summary>
@@ -41,7 +66,7 @@ namespace ProfielWerkstuk
 		{
 			// Create a new SpriteBatch, which can be used to draw textures.
 			spriteBatch = new SpriteBatch(GraphicsDevice);
-
+			font = Content.Load<SpriteFont>("Calibri16");
 			// TODO: use this.Content to load your game content here
 		}
 
@@ -52,6 +77,9 @@ namespace ProfielWerkstuk
 		protected override void UnloadContent()
 		{
 			// TODO: Unload any non ContentManager content here
+			grid.Dispose();
+			spriteBatch.Dispose();
+			graphics.Dispose();
 		}
 
 		/// <summary>
@@ -61,10 +89,43 @@ namespace ProfielWerkstuk
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update(GameTime gameTime)
 		{
-			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			KeyboardState state = Keyboard.GetState();
+			MouseState mouseState = Mouse.GetState();
+			if (state.IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
 				Exit();
 
 			// TODO: Add your update logic here
+
+			if(System.Windows.Forms.Form.ActiveForm == (System.Windows.Forms.Control.FromHandle(Window.Handle) as System.Windows.Forms.Form))
+			{
+				if(oldState != null)
+				{
+					if (mouseState.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+					{
+						if(oldState.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Released)
+						{
+							previousMouse.X = mouseState.X;
+							previousMouse.Y = mouseState.Y;
+						}
+						else if(oldState.LeftButton == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+						{
+							camera.Move((previousMouse - new Vector2(mouseState.X, mouseState.Y))/camera.Zoom);
+							previousMouse.X = mouseState.X;
+							previousMouse.Y = mouseState.Y;
+						}
+					}
+
+					int deltaScroll = mouseState.ScrollWheelValue - oldState.ScrollWheelValue;
+					if (deltaScroll != 0)
+						if (deltaScroll < 0)
+							camera.ZoomOut(0.1f*(-deltaScroll/120f));
+						else
+							camera.ZoomIn(0.1f*(deltaScroll/120f));
+				}
+				//Update old mousestate
+				oldState = mouseState;
+			}
+			
 
 			base.Update(gameTime);
 		}
@@ -75,9 +136,20 @@ namespace ProfielWerkstuk
 		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw(GameTime gameTime)
 		{
-			GraphicsDevice.Clear(Color.CornflowerBlue);
+			GraphicsDevice.Clear(Color.WhiteSmoke);
+			// TODO: Add your drawing code 
 
-			// TODO: Add your drawing code here
+			Vector2 screenOffset = new Vector2(GraphicsDevice.DisplayMode.Width / 2, GraphicsDevice.DisplayMode.Height/2);
+
+			spriteBatch.Begin(transformMatrix: camera.GetViewMatrix());
+			grid.DrawGridLines(spriteBatch);
+			grid.DrawGridSquares(spriteBatch);
+			spriteBatch.End();
+
+			//Draw UI
+			spriteBatch.Begin();
+			UI.DrawButton(spriteBatch, "Button", 28f, new Vector2(100, 50), font);
+			spriteBatch.End();
 
 			base.Draw(gameTime);
 		}
---
+++ ProfielWerkstuk/Scripts/UI.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Shapes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProfielWerkstuk.Scripts
+{
+	public class UI
+	{
+		public static void DrawButton(SpriteBatch spriteBatch, string text, float fontSize, Vector2 position, SpriteFont font, Vector2 padding)
+		{
+			//Measure the length in pixels of the string
+			Vector2 length = font.MeasureString(text);
+			Vector2 buttonSize = length + (2 * padding) - new Vector2(0, fontSize / 3.2f);
+
+			MonoGame.Extended.Shapes.SpriteBatchExtensions.FillRectangle(spriteBatch, position - padding, buttonSize, new Color(147, 188, 255));
+			MonoGame.Extended.Shapes.SpriteBatchExtensions.DrawRectangle(spriteBatch, position - padding, buttonSize, new Color(86, 148, 255), 4);
+			spriteBatch.DrawString(font, text, position, new Color(80, 97, 127));
+		}
+
+		public static void DrawButton(SpriteBatch spriteBatch, string text, float fontSize, Vector2 position, SpriteFont font)
+		{
+			//Basic padding
+			Vector2 padding = new Vector2(5, 5);
+
+			//Measure the length in pixels of the string
+			Vector2 length = font.MeasureString(text);
+			Vector2 buttonSize = length + (2 * padding) - new Vector2(0, fontSize / 3.2f);
+
+			MonoGame.Extended.Shapes.SpriteBatchExtensions.FillRectangle(spriteBatch, position - padding, buttonSize, new Color(147, 188, 255));
+			MonoGame.Extended.Shapes.SpriteBatchExtensions.DrawRectangle(spriteBatch, position - padding, buttonSize, new Color(86, 148, 255), 4);
+			spriteBatch.DrawString(font, text, position, new Color(80, 97, 127));
+		}
+	}
+}
